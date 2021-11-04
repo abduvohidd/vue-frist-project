65b022fccee94901708b72ba0ae2e5fa0aafc6e4
Abduvohid <vohid4970@gmail.com>
2021-11-04 12:38:12 +0500

    Todolist style

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -5,7 +5,7 @@
             <strong>{{todo.id}}</strong>
             {{todo.title}}
         </span>
-        <button>&times;</button>
+        <button class="rm">&times;</button>
     </li>
 </template>
 
@@ -21,3 +21,20 @@ export default{
     }
 }
 </script>
+
+
+<style scoped>
+    li{
+        border:1px solid #ccc;
+        display: flex;
+        justify-content: space-between;
+        padding: 0.5rem 2rem;
+        margin-bottom: 1rem;
+    }
+    .rm{
+        background-color: red;
+        color: #fff;
+        border-radius: 50%;
+        font-weight: bold;
+    }
+</style>
