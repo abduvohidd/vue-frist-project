eadd2dadd17c107f5523d32a60a59a12174b7518
Abduvohid <vohid4970@gmail.com>
2021-11-04 12:31:44 +0500

    Frist commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <li>
+        <span>
+            <input type="checkbox">
+            <strong>{{todo.id}}</strong>
+            {{todo.title}}
+        </span>
+        <button>&times;</button>
+    </li>
+</template>
+
+
+<script>
+
+export default{
+    props:{
+        todo: {
+            type: Object,
+            required: false
+        }
+    }
+}
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <ul>
+            <TodoItem
+                v-for="todo in todos"
+                :todo="todo"
+                :key="todo.id"
+            />
+        </ul>
+    </div>
+</template>
+
+<script>
+import TodoItem from '@/components/TodoItem'
+export default {
+    props:['todos'],
+    components:{
+        TodoItem
+    }
+}
+</script>
+
+
+<style scoped>
+    ul{
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+</style>
